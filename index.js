5ac8b67ddc15463ab3fdc9fcbbc8a9cbd5f3bd70
mukeshgandha-source <mukesh.gandha@gmail.com>
2026-04-13 09:37:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// 🚀 Ye line humne add ki hai Bootstrap ke liye
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
